b29decd43ee875b6f0a12ec965d1d6aceac7015e
hiroshige.negishi <hirosige1@gmail.com>
2020-03-22 18:15:36 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   $.ajax({
     type: "GET",
-    url: "http://127.0.0.1:5500/public/countries.json",
+    url: "/public/countries.json",
     success: async function(data) {
       for (country of data.data) {
         const $option = $("<option>")
